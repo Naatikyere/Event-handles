90a9c56100286bf4c48aff58e42fd85db4d8f681
prosperking <naatikyereprosper@gmail.com>
2023-02-26 10:28:59 +0100

    Done with the second part that deletes clicked list

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const List = document.querySelector('.list');
 List.addEventListener('click',
 (e)=>(
     alert(e.target.className)
-    ))
+))
     /* it is automated to just give you the name of
      the color clicked without
     having to name each of them seperately */
 
+/* Images Hovering 
+************* 
+   This section actually removes an item from the list*/
 
+document.querySelector('.list-hover').addEventListener('click',
+    function (d) {
+        console.dir(d)
+        if (d.target.tagName === 'IMG') {
+            
+ var removeList = d.target.parentNode;
+ removeList.parentNode.removeChild(removeList);
+        }
+    
+},false)
