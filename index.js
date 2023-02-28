14987fdb62fafde3487ffb1c296e98dbc5cfef76
prosperking <naatikyereprosper@gmail.com>
2023-02-28 10:35:32 +0100

    Done with hovering the list items

--- index.js
+++ index.js
@@ -13,11 +13,60 @@ List.addEventListener('click',
 
 document.querySelector('.list-hover').addEventListener('click',
     function (d) {
-        console.dir(d)
+      
         if (d.target.tagName === 'IMG') {
-            
- var removeList = d.target.parentNode;
+            const  howMany = document.querySelectorAll('.list-hover li').length;
+            if (howMany > 1) {
+                var removeList = d.target.parentNode;
  removeList.parentNode.removeChild(removeList);
+            }
         }
     
-},false)
+    })
+    /******************Mouse Hover section */
+document.querySelector('.getTheImage').addEventListener('mouseover',(e)=>{
+    if(e.target.tagName === 'IMG'){
+      var myElement=document.createElement('div'); //create a new element
+        myElement.className ='preview'; // give the element a class name
+        e.target.parentNode.appendChild(myElement); //add the element to an existin element in the dom 
+    var myImg = document.createElement('img') //create the main image element
+    myImg.src = e.target.src;
+        myElement.appendChild(myImg);
+        
+        e.target.addEventListener('mouseout', function remover(d){
+            var myNode = d.target.parentNode.querySelector('div.preview');// calling the selector from the 'd' function 
+            myNode.parentNode.removeChild(myNode);
+        e.target.removeEventListener('mouseout', remover)
+         })
+
+    }
+})
+
+/**************Mouse Click section */
+document.querySelector('.getTheImageByClick').addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    if(e.target.tagName === 'IMG'){
+      var myElement=document.createElement('div'); //create a new element
+        myElement.className ='preview'; // give the element a class name
+        e.target.parentNode.appendChild(myElement); //add the element to an existin element in the dom 
+    var myImg = document.createElement('img') //create the main image element
+        myImg.src = e.target.src;
+        
+         myElement.style.left = e.offsetX + 20 + 'px'
+            myElement.style.top = e.offsetY + 10 + 'px'
+        
+        myElement.appendChild(myImg);
+        
+        e.target.addEventListener('mouseout', function remover(d){
+            var myNode = d.target.parentNode.querySelector('div.preview');// calling the selector from the 'd' function 
+            myNode.parentNode.removeChild(myNode);
+        e.target.removeEventListener('mouseout', remover)
+        })
+        
+        e.target.addEventListener('mousemove', function (m) {
+            myElement.style.left = m.offsetX + 20 + 'px'
+            myElement.style.top = m.offsetY + 10 + 'px'
+        })
+
+    }
+})
